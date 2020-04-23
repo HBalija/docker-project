1d136818a9a6733f4c3471bf502e458368cd5b78
HBalija <balija.hrvoje@gmail.com>
2020-04-23 16:27:56 +0200

    Add React client code

---
+++ client/src/components/OtherPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Link } from 'react-router-dom'
+
+export default () => {
+  return  (
+    <div>
+      <p>Other project page</p>
+      <Link to="/">Go back home</Link>
+    </div>
+  )
+}
